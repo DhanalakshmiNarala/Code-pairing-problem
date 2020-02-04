e1774ad36a445df8394e05c0f472dc129cfa877e
Dhanalakshmi Narala <dhanalakshmi.narala@gmail.com>
2020-02-04 23:32:38 +0530

    Add Player and Cricket classes

--- src/cricket.js
+++ src/cricket.js
@@ -1,1 +1,84 @@
-module.exports = null;
+const { Player, getRandomNumber } = require('../src/player');
+
+class Cricket {
+  players = [];
+  oversRemaining;
+  runsNeeded;
+  currentStriker;
+
+  setOversRemaining(overs) {
+    this.oversRemaining = overs;
+  }
+
+  setRunsNeeded(runs) {
+    this.runsNeeded = runs;
+  }
+
+  setPlayersRemaining(players) {
+    for (let player of players) {
+      this.insertPlayer(player);
+    }
+  }
+
+  insertPlayer(player) {
+    let newPlayer = new Player(player.name, player.scoresProbability);
+    this.players.push(newPlayer);
+  }
+
+  changeStriker(isOverCompleted = false, score) {
+    if (isOverCompleted || score === 1 || score === 3 || score === 5) {
+      let playerIndex = getRandomNumber(0, this.players.length);
+      this.currentStriker = this.players[playerIndex];
+    }
+  }
+
+  getMatchCommentary() {
+    console.log('Sample commetary');
+    let runs = null;
+    for (let overs = 0; overs < this.oversRemaining; overs++) {
+      console.log(
+        `\n${this.oversRemaining - overs} overs left. ${
+          this.runsNeeded
+        } runs to win\n`
+      );
+
+      this.changeStriker(true, runs);
+      for (let balls = 1; balls <= 6; balls++) {
+        this.changeStriker(balls === 6, runs);
+        runs = this.currentStriker.play();
+        if (runs !== 7) {
+          console.log(
+            `${overs}.${balls}\t${this.currentStriker.name} scores ${runs} runs`
+          );
+        }
+      }
+    }
+  }
+
+  getMatchResult() {
+    this.getMatchCommentary();
+    console.log('------------------ Final Match results ---------');
+    for (let player of this.players) {
+      console.log(
+        `${player.name} - ${player.totalRuns}(${player.totalBalls} balls)`
+      );
+    }
+  }
+
+  main() {
+    this.setOversRemaining(4);
+    this.setRunsNeeded(40);
+    this.setPlayersRemaining([
+      { name: 'Kirat boli', scoresProbability: [5, 30, 25, 10, 15, 1, 9, 5] },
+      { name: 'N.S Nodhi', scoresProbability: [10, 40, 20, 5, 10, 1, 4, 10] },
+      { name: 'R Rumrah', scoresProbability: [20, 30, 15, 5, 5, 1, 4, 20] },
+      { name: 'Shashi Henra', scoresProbability: [30, 25, 5, 0, 5, 1, 4, 30] }
+    ]);
+    this.getMatchResult();
+  }
+}
+
+let cricket = new Cricket();
+cricket.main();
+
+module.exports = Cricket;
---
+++ src/player.js
@@ -0,0 +1,42 @@
+class Player {
+  constructor(name, probArr) {
+    this.name = name;
+    this.scoresProbability = probArr;
+    this.totalBalls = 0;
+    this.totalRuns = 0;
+  }
+
+  getScore() {
+    let cumulativeSumArr = [];
+    for (let i = 0; i < this.scoresProbability.length; i++) {
+      if (i === 0) {
+        cumulativeSumArr[i] = this.scoresProbability[i];
+      } else {
+        cumulativeSumArr[i] =
+          cumulativeSumArr[i - 1] + this.scoresProbability[i];
+      }
+    }
+    let randomScore = getRandomNumber(0, 100);
+    for (let index = 0; index < cumulativeSumArr.length; index++) {
+      if (randomScore < cumulativeSumArr[index]) {
+        return index;
+      }
+    }
+    throw 'Error in calculating player score';
+  }
+
+  play() {
+    this.totalBalls += 1;
+    let runs = this.getScore();
+    if (runs !== 7) {
+      this.totalRuns += runs;
+    }
+    return runs;
+  }
+}
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+module.exports = { Player, getRandomNumber };
