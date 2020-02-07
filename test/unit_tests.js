44951e32a19c21398a9852e24a01f95166137e02
Dhanalakshmi Narala <dhanalakshmi.narala@gmail.com>
2020-02-07 11:01:20 +0530

    Add unit tests for 'getNewStriker'

--- test/unit_tests.js
+++ test/unit_tests.js
@@ -2,22 +2,43 @@ const Cricket = require('../src/cricket');
 const should = require('chai').should();
 
 describe('Unit tests', () => {
-  let cricket = new Cricket();
+  describe('get new striker', () => {
+    let players = [
+      { name: 'Kirat boli', scoresProbability: [5, 30, 25, 10, 15, 1, 9, 5] },
+      { name: 'N.S Nodhi', scoresProbability: [10, 40, 20, 5, 10, 1, 4, 10] },
+      { name: 'R Rumrah', scoresProbability: [20, 30, 15, 5, 5, 1, 4, 20] },
+      { name: 'Shashi Henra', scoresProbability: [30, 25, 5, 0, 5, 1, 4, 30] }
+    ];
+    let cricket = new Cricket(4, 40, players);
 
-  before(() => {
-    cricket.setOversReaming(4);
-    cricket.setRunsNeeded(40);
-    cricket.setPlayersRemaining([
-      { name: 'Kirat boli', probability: [5, 30, 25, 10, 15, 1, 9, 5] },
-      { name: 'N.S Nodhi', probability: [10, 40, 20, 5, 10, 1, 4, 10] },
-      { name: 'R Rumrah', probability: [20, 30, 15, 5, 5, 1, 4, 20] },
-      { name: 'Shashi Henra', probability: [30, 25, 5, 0, 5, 1, 4, 30] }
-    ]);
+    it('should return new striker (R Rurah)', () => {
+      let expectedResult = {
+        name: 'R Rumrah',
+        scoresProbability: [20, 30, 15, 5, 5, 1, 4, 20],
+        totalBalls: 0,
+        totalRuns: 0,
+        isEliminated: false
+      };
+      cricket.getNewStriker().should.deep.equal(expectedResult);
+    });
 
-    it('should return commentry for the cricket', () => {
-      let expectedResult =
-        'Bengaluru won by 1 wicket and 2 balls remaining\n\nKirat Boli - 12 (6 balls)\nNS Nodhi - 25 (11 balls)\nR Rumrah - 2* (3 balls)\nShashi Henra - 2* (2 balls)';
-      cricket.getMatchResult().should.equal(expectedResult);
+    it('should return new striker (N.S Nodhi)', () => {
+      let expectedResult = {
+        name: 'N.S Nodhi',
+        scoresProbability: [10, 40, 20, 5, 10, 1, 4, 10],
+        totalBalls: 0,
+        totalRuns: 0,
+        isEliminated: false
+      };
+      cricket.getNewStriker().should.deep.equal(expectedResult);
+    });
+
+    it('should return Not players left when all players eliminated', () => {
+      cricket.players.forEach(player => {
+        player.isEliminated = true;
+      });
+
+      cricket.getNewStriker().should.equal('No players left');
     });
   });
 });
